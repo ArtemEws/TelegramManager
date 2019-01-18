0213f1032a12b1cb7cd30cddf0a80316dea7e990
Viacheslav Surkov <surokpro@gmail.com>
2019-01-18 21:07:26 +0300

    Добавлена проверка на админа

---
+++ LibraryCover/src/org/drinkless/tdlib/Main.java
@@ -0,0 +1,4 @@
+package org.drinkless.tdlib;
+
+public class Main {
+}
--- LibraryCover/src/org/drinkless/tdlib/apihelper/Logger.java
+++
@@ -1,7 +0,0 @@
-package org.drinkless.tdlib.apihelper;
-
-public class Logger {
-    public static void log(String mes) {
-        System.out.println(mes);
-    }
-}
