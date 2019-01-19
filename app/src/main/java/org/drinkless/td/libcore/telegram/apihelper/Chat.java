46a0f3b3d309310263c0426166e5ee8397bfeb78
Artem Evsikov <anon.mail.ya@yandex.ru>
2019-01-19 21:38:36 +0300

    merge

---
+++ app/src/main/java/org/drinkless/td/libcore/telegram/apihelper/Chat.java
@@ -0,0 +1,61 @@
+package org.drinkless.td.libcore.telegram.apihelper;
+
+import org.drinkless.td.libcore.telegram.TdApi;
+
+public class Chat {
+
+    static String BASIC_GROUP = "BasicGroup";
+    static String PRIVATE = "Private";
+    static String SECRET = "Secret";
+    static String SUPER_GROUP = "SuperGroup";
+    static String NON_SPECIFIED = "NonSpecified";
+
+    TdApi.Chat chat;
+
+    Chat() {
+        this.chat = null;
+    }
+
+    Chat(TdApi.Chat chat) {
+        this.chat = chat;
+    }
+
+    public String getTitle() {
+        return chat.title;
+    }
+
+    public String getType() {
+        TdApi.ChatType type = chat.type;
+        switch (type.getConstructor()) {
+            case TdApi.ChatTypeBasicGroup.CONSTRUCTOR:
+                return BASIC_GROUP;
+            case TdApi.ChatTypePrivate.CONSTRUCTOR:
+                return PRIVATE;
+            case TdApi.ChatTypeSecret.CONSTRUCTOR:
+                return SECRET;
+            case TdApi.ChatTypeSupergroup.CONSTRUCTOR:
+                return SUPER_GROUP;
+            default:
+                return NON_SPECIFIED;
+        }
+    }
+
+    public boolean isSuperGroup() {
+        return chat.type.getConstructor() == TdApi.ChatTypeSupergroup.CONSTRUCTOR;
+    }
+
+    public boolean isChannel() {
+        if (chat.type.getConstructor() == TdApi.ChatTypeSupergroup.CONSTRUCTOR) {
+            return ((TdApi.ChatTypeSupergroup)chat.type).isChannel;
+        }
+        return false;
+    }
+
+    public FileManager.File getPhotoFile() {
+        return new FileManager.File(chat.photo.small);
+    }
+
+    public boolean hasPhoto() {
+        return chat.photo != null;
+    }
+}
