73d31d84f4190d5c9f9bef5778810fcb80da6397
Artem <stratosphere665@gmail.com>
2019-01-24 09:45:06 +0300

    Для сашЫ

--- app/src/main/java/org/telegram/telegrammanager/MainActivity.java
+++ app/src/main/java/org/telegram/telegrammanager/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
